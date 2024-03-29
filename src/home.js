f9fd71e5faeae499dd656c82b0b537f261efaf00
Your Name <you@example.com>
2024-03-29 19:50:06 +0000

    react tutorial 29032024

--- src/home.js
+++ src/home.js
@@ -1,15 +1,14 @@
-import { useState, useEffect } from "react";
 import BlogList from "./blogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
+  const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
 
   return ( 
     <div className="home">
-      {error && <d>{error}</d>}
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+      {blogs && <BlogList blogs={blogs} />}
     </div>
    );
 }
